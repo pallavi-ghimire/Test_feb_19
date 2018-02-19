e9c85500073a3ef801f6683d2de03409354e506a
palavi-ghimiregit config --global user.name pallavi-ghimire <pallavi.ghimire@deerwalk.edu.np>
2018-02-19 11:40:59 +0545

    First commit

--- Test.py
+++ Test.py
@@ -18,4 +18,21 @@ print(r.getArea(), r.getPerimeter())
 
 """Q2. Create a class Circle which has a class variable PI with the value=22/7. Make two methods getArea and getCircumference inside 
 this Circle class. Which when invoked returns area and circumference of each circle instances."""
+class Circle:
+    PI = 22/7
+    def __init__(self, radius):
+        self.radius = radius
+    def getArea(self):
+        area = Circle.PI * self.radius * self.radius
+        return area
+    def getCircumference(self):
+        cir = Circle.PI * 2 * self.radius
+        return cir
+
+c = Circle(3)
+print(c.getArea(), c.getCircumference())
+
+
+"""Create a Employee class and initialize it with first_name, last_name and salary. Also, it has a derived attribute called email, which is self generated when instance is created. Now, make methods to :
+      a. Display - It should display all information of the employee instance."""
 
